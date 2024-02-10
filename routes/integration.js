56a4605d17f47d4f5a46b908c227ae7b8ead6974
cdlavacudeg <cristianlav10@gmail.com>
2024-02-09 19:20:12 -0500

    [Fix] Verbs for integration PUT to POST

--- routes/integration.js
+++ routes/integration.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.put('/companies', async function (req, res, next) {
+router.post('/companies', async function (req, res, next) {
   return res.json({
     message: 'PUT /integration/companies',
     body: req.body,
   });
 });
 
-router.put('/contacts', async function (req, res, next) {
+router.post('/contacts', async function (req, res, next) {
   return res.json({
     message: 'PUT /integration/contacts',
     body: req.body,
